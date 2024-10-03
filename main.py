d79d12127ab78150fc4617d8b6abd1d5b0bfd12b
HopenHeimer <ghost0reboot@gmail.com>
2024-10-03 06:22:00 +0100

    Create main.py
    base of my None project

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import random
+import socket
+
+
+class Exer_Backdoor_Exer_A :
+  def __init__(self) :
+    self.local_ip = None
+    self.local_port = None
+    #chargement des informations de l'hote distant
+    self.reverse_host = None
+    Self.reverse_host_port = None
+    #on genere l'object de connexion
+    self.connect_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def bind_ip_address(self):
+  try :
+    pass
+  except :
+    pass
+  finally :
+    pass
